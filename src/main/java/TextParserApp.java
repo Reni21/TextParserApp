469fb25e3b9179b65c488de428d23cb20f2ccdcb
Reni21 <r.phoenixart@gmail.com>
2019-10-07 15:13:53 +0300

    Modify main app class

--- src/main/java/TextParserApp.java
+++ src/main/java/TextParserApp.java
@@ -13,7 +13,8 @@ import java.util.stream.Collectors;
 public class TextParserApp {
     private static String INTRO = "\033[93mHello! It is the app for text modification.\n" +
             "You can cut some part from every sentences. Choose two symbols which will be used for it (inclusive).\n" +
-            "Use \"--q\" command for quite the app\033[0m";
+            "\"--show\"     show source text\n" +
+            "\"--q\"        command for quite the app\033[0m";
 
     private TextService textService;
 
@@ -22,8 +23,7 @@ public class TextParserApp {
     }
 
     public static void main(String[] args) {
-        SentenceService sentenceService = new SentenceService();
-        TextService textService = new TextService(sentenceService);
+        TextService textService = new TextService(new SentenceService());
         TextParserApp app = new TextParserApp(textService);
 
         try (Scanner scanner = new Scanner(System.in)) {
@@ -40,26 +40,44 @@ public class TextParserApp {
 
         while (true) {
             try {
-                System.out.println("\nEnter char to start cut from:");
-                String left = scanner.nextLine();
-                validateInput(left);
-                System.out.println("Enter char to start cut before:");
-                String right = scanner.nextLine();
-                validateInput(right);
-
-                Text transformedText = textService.cutSubstringInEverySentence(text, left.charAt(0), right.charAt(0));
-                System.out.println(transformedText);
+                requestAndProcessInput(scanner, text);
             } catch (IllegalArgumentException ex) {
                 System.out.println(ex.getMessage());
             }
         }
     }
 
-    private void validateInput(String input) {
-        if (input.equals("--q")) {
-            System.out.println("You just quite the app...");
-            System.exit(0);
+    private void requestAndProcessInput(Scanner scanner, Text text){
+        System.out.println("\nEnter char to start cut from:");
+        String leftTerminal = scanner.nextLine();
+
+        String rightTerminal = null;
+        if(handleInput(leftTerminal, text)){
+            System.out.println("Enter char to start cut before:");
+            rightTerminal = scanner.nextLine();
         }
+        if (rightTerminal != null && handleInput(rightTerminal, text)) {
+            Text transformedText = textService.cutSubstringInEverySentence(text, leftTerminal.charAt(0), rightTerminal.charAt(0));
+            System.out.println(transformedText);
+        }
+    }
+
+    private boolean handleInput(String input, Text text) {
+        switch (input){
+            case "--q":
+                System.out.println("You just quite the app...");
+                System.exit(0);
+                break;
+            case "--show":
+                System.out.println(text);
+                return false;
+            default:
+                validateInput(input);
+        }
+        return true;
+    }
+
+    private void validateInput(String input) {
         if (input.isEmpty()) {
             throw new IllegalArgumentException("Please, enter a char");
         }
@@ -68,10 +86,10 @@ public class TextParserApp {
         }
     }
 
-    private Text generateText() throws IOException, URISyntaxException{
+    public Text generateText() throws IOException, URISyntaxException {
         TextParser textParser = new TextParser();
-        String textStr = Files.lines(Paths.get(ClassLoader.getSystemResource("text.txt").toURI()))
+        String textSrc = Files.lines(Paths.get(ClassLoader.getSystemResource("text.txt").toURI()))
                 .collect(Collectors.joining("\n"));
-        return textParser.parseText(textStr);
+        return textParser.parseText(textSrc);
     }
 }
