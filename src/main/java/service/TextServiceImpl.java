9617d7c22c75a399e12240e83dda6fc70e70a06f
Reni21 <r.phoenixart@gmail.com>
2019-10-08 13:57:48 +0300

    Add interface for services

---
+++ src/main/java/service/TextServiceImpl.java
@@ -0,0 +1,34 @@
+package service;
+
+import entity.Paragraph;
+import entity.Sentence;
+import entity.Text;
+import exception.SentenceDoesNotContainRequiredElementException;
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+public class TextServiceImpl implements TextService {
+    private SentenceService sentenceService;
+
+    public Text deleteSubstringInEverySentence(Text src, char from, char to) {
+        if (src == null) {
+            throw new IllegalArgumentException("Source text is null.");
+        }
+        List<Paragraph> transformedParagraphs = src.getParagraphs().stream().map(paragraph -> {
+            List<Sentence> transformedSentences = paragraph.getSentences().stream().map(sentence -> {
+                try {
+                    return sentenceService.deleteSequenceBetweenChars(sentence, from, to);
+                } catch (SentenceDoesNotContainRequiredElementException ex) {
+                    return sentenceService.cloneSentence(sentence);
+                }
+            }).filter(sentence -> !sentence.getTokens().isEmpty())
+                    .collect(Collectors.toList());
+            return new Paragraph(transformedSentences);
+        }).filter(paragraph -> !paragraph.getSentences().isEmpty())
+                .collect(Collectors.toList());
+        return new Text(transformedParagraphs);
+    }
+}
